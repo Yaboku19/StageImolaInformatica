57caa3d32e0e02c20cd606ca3c53411b604e22de
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-09-09 13:14:52 +0200

    first implementation of executor

--- src/main/java/it/unibo/tirocinio/martelli/execute/impl/PasteBinCrawler.java
+++ src/main/java/it/unibo/tirocinio/martelli/execute/impl/PasteBinCrawler.java
@@ -8,25 +8,36 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.unibo.tirocinio.martelli.execute.api.Crawler;
 import it.unibo.tirocinio.martelli.typedata.PastebinScrapingItem;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @SuppressWarnings("unchecked")
 public class PasteBinCrawler extends Crawler {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     @Override
     public void execute(final Map<String, Object> config) throws IOException {
         setConnectionTimeout((Integer)((Map<String, Object>)config.get("timeout"))
                             .get("connect"));
         setReadTimeout((Integer)((Map<String, Object>)config.get("timeout"))
                             .get("read"));
-
-        List<PastebinScrapingItem> list = getScrapingItem(doGetRequest((String) config.get("url")));
+        final List<PastebinScrapingItem> list = getScrapingItem(doGetRequest((String) config.get("url")));
         final List<String> insideList = new ArrayList<>();
-        for(PastebinScrapingItem item : list) {
-            insideList.add(doGetRequest(item.getScrapeUrl()));
-            /*
-             * waiting time Executor
-             */
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            scheduler.schedule(getReader(insideList, list.get(i)), i * getWaitingTime(config), SECONDS);
         }
-        System.out.println(insideList);
+        scheduler.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(insideList);
+            }
+        }, list.size() * getWaitingTime(config), SECONDS);
+        
+    }
+
+    private int getWaitingTime(final Map<String, Object> config) {
+        return (Integer)config.get("waitingTime");
     }
 
     private List<PastebinScrapingItem> getScrapingItem(final String data) throws IOException {
@@ -38,6 +49,20 @@ public class PasteBinCrawler extends Crawler {
             list.add(object);
         }
         return list;
+    }
+
+    private Runnable getReader(final List<String> insideList, final PastebinScrapingItem item) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    insideList.add(doGetRequest(item.getScrapeUrl()));
+                    System.out.println("fatto");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     @Override
