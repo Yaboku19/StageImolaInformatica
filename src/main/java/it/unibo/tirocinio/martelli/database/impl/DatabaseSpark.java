0db5b9f3941361c33d7d3b92d40720687c33724f
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-09-15 20:16:44 +0200

    modify the configuration of jacson on pom.xml

--- src/main/java/it/unibo/tirocinio/martelli/database/impl/DatabaseSpark.java
+++ src/main/java/it/unibo/tirocinio/martelli/database/impl/DatabaseSpark.java
@@ -1,5 +1,9 @@
 package it.unibo.tirocinio.martelli.database.impl;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import it.unibo.tirocinio.martelli.database.api.Database;
 
@@ -11,10 +15,18 @@ public class DatabaseSpark implements Database{
      }
 
      public static void main(String[] args) {
-          final SparkSession spark = SparkSession.builder()
-          .appName("Prova")
-          .master("local[*]")
-          .getOrCreate();
+               try(final var spark = SparkSession.builder()
+                                   .appName("Prova")
+                                   .master("local[*]")
+                                   .getOrCreate();
+                    final var  sc = new JavaSparkContext(spark.sparkContext())) {
+               final var data = Stream.iterate(1, x -> x + 1)
+                    .limit(5)
+                    .collect(Collectors.toList());
+               System.out.println(data);
+               final var myrdd = sc.parallelize(data);
+               System.out.println(myrdd.count());
+          }
      }
      
 }
